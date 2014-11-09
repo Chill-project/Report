c4f50d54d7539debd955b61da899a2fc0cbecb28
Marc Ducobu <marc@champs-libres.coop>
2014-11-09 19:38:26 +0100

    Adding CFGroup for reports

---
+++ DataFixtures/ORM/LoadCustomFieldsGroup.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * Chill is a software for social workers 
+ *
+ * Copyright (C) 2014, Champs Libres Cooperative SCRLFS, <http://www.champs-libres.coop>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Chill\PersonBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Chill\CustomFieldsBundle\Entity\CustomFieldsGroup;
+
+/**
+ * Load CustomFieldsGroup for Report into database
+ */
+class LoadCustomFieldsGroup extends AbstractFixture implements OrderedFixtureInterface {
+    public function getOrder() {
+        return 15000;
+    }
+    
+    public function load(ObjectManager $manager) {
+        echo "loading customFieldsGroup...\n";
+
+        for($i=0; $i <= 3; $i++) {
+            $cFGroup = (new CustomFieldsGroup())
+                ->setName(array('fr' => 'CFGroup Report ' . $i))
+                ->setEntity('Chill\ReportBundle\Entity\Report');
+
+            $manager->persist($cFGroup);
+        }
+        $manager->flush();
+    }
+}
